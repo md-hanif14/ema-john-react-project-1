bba4e8f5cc44126b90a9922a786ae5790938af62
md-hanif14 <hanifuzzaman.programminghero@gmail.com>
2023-03-31 02:36:52 +0600

    order part added

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -6,11 +6,19 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
     useEffect( () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     } , [])
+
+    const handleAddToCart = (product) => {
+        // console.log(product);
+        const newCart = [...cart, product]
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-container'>
             <div className="product-container">
@@ -18,11 +26,13 @@ const Shop = () => {
                     products.map(product => <Product
                     key={product.id}
                     product={product}
+                    handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order summery</h4>
+                <p>Selected Items: {cart.length}</p>
 
             </div>
         </div>
